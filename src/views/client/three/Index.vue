d550de6f8c477f5269993aa503e865fad3f0378c
JiasNight <Jias96@163.com>
2021-12-29 21:47:25 +0800

    修改添加gltf模型

--- src/views/client/three/Index.vue
+++ src/views/client/three/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="three-container">
-    <div ref="threeDom" class="three-main"></div>
+    <div ref="threeDom" class="three-main" id="three-box"></div>
   </div>
 </template>
 
@@ -11,148 +11,210 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DragControls } from 'three/examples/jsm/controls/DragControls';
 import gsap from 'gsap';
+import { CreateDivThree, LoadGltfModel } from './utils/threeTool';
 
 onMounted(() => {
   initCurrentThree();
-  loadGltfModel();
-  renderThree();
+  // loadGltfModel();
+  // renderThree();
 });
-
-const Scene = new THREE.Scene();
-const Camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-const Renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  alpha: true //开启alpha
-});
-// 控制器
-const Controls = new OrbitControls(Camera, Renderer.domElement);
-//gltfLoader
-const Gltfloader = new GLTFLoader();
-// 注意:此处为threejs的DOM,需要将threejs的场景渲染进去
-const threeDom = ref(null);
-
-// 首页进入相机的视角,这个视角可以在三维模型中建立一个摄像机获取摄像机的坐标,如C4D,非常准确.
-const cameraPosition = {
-  x: 0,
-  y: 700,
-  z: 0
-};
-const cameraLookat = {
-  x: 0,
-  y: 0,
-  z: 1
-};
-
-// 声明一个方法传入参数可以在不同的地方调用相机
-const cameraReset = (position, lookAt, time = 1) => {
-  gsap.to(Camera.position, {
-    x: position.x,
-    y: position.y,
-    z: position.z,
-    duration: time,
-    ease: 'power4.out'
-    // onComplete: function () {
-    // 这是相机运动完成的回调,可以执行其他的方法.
-    // }
-  });
-  gsap.to(Camera.lookAt, {
-    x: lookAt.x,
-    y: lookAt.y,
-    z: lookAt.z,
-    duration: time,
-    ease: 'power4.out'
-  });
-  gsap.to(Controls.target, {
-    x: lookAt.x,
-    y: lookAt.y,
-    z: lookAt.z,
-    duration: time,
-    ease: 'power4.out'
-  });
-};
 
 const initCurrentThree = () => {
-  // 点光源
-  const point = new THREE.PointLight(0xffffff, 1);
-  point.position.set(10, 600, -40); // 点光源位置
-  // Scene.position.set(0, 15, 0); // 场景位置
-  // Scene.add(point); // 点光源添加到场景中
-  // 环境光
-  const ambient = new THREE.AmbientLight(0x444444, 1);
-  Scene.add(ambient);
-  // 辅助坐标系
-  const axesHelper = new THREE.AxesHelper(500);
-  // Scene.add(axesHelper);
-  // 修改相机,场景的参数
-  // Camera.position.set(18, 364, 397);
-  // Camera.lookAt(0, 0, 0);
-  // Camera.up.x = 0; //相机以哪个方向为上方
-  // Camera.up.y = 1;
-  // Camera.up.z = 1;
-  Controls.target = new THREE.Vector3(0, 0, 0);
-  // 使动画循环使用时阻尼或自转 意思是否有惯性
-  Controls.enableDamping = true;
-  // 动态阻尼系数 就是鼠标拖拽旋转灵敏度
-  Controls.dampingFactor = 0.04;
-  // 是否可以旋转
-  Controls.enableRotate = false;
-  // 是否可以缩放与速度
-  Controls.enableZoom = false;
-  // 设置相机距离原点的最远距离
-  Controls.minDistance = 1;
-  // 设置相机距离原点的最远距离
-  Controls.maxDistance = 2000;
-  // 是否开启右键拖拽
-  Controls.enablePan = false;
-  //render的相关设置
-  Renderer.setPixelRatio(window.devicePixelRatio);
-  Renderer.setSize(window.innerWidth, window.innerHeight);
-  Renderer.inputEncoding = true;
-  Renderer.outputEncoding = THREE.sRGBEncoding;
-  Renderer.setClearColor(0xd0d0d0, 1);
-  // 将renderer渲染进DOM里面
-  threeDom.value.appendChild(Renderer.domElement);
+  const cdt = new CreateDivThree('#three-box');
+  cdt.init();
+  const modelPath: string = '/src/assets/gltf/';
+  const modelName: string = 'scene.gltf';
+  const scene: any = cdt.scene;
+  const camera: any = cdt.camera;
+  const controls: any = cdt.controls;
+  window.camera = camera;
+  window.controls = controls;
+  console.log(window);
+  const gltfModel = new LoadGltfModel(modelPath, modelName, scene, camera);
+  gltfModel.loadModel();
+  const modelScene = gltfModel.modelScene;
+  // gltfModel.adjustModel('#three-box', modelScene);
+  // 首页进入相机的视角,这个视角可以在三维模型中建立一个摄像机获取摄像机的坐标,如C4D,非常准确.
+  const cameraPosition = {
+    x: 2,
+    y: 320,
+    z: 350
+  };
+  const cameraLookat = {
+    x: 0,
+    y: 200,
+    z: 100
+  };
+  cdt.cameraReset(cameraPosition, cameraLookat);
 };
 
-// 设置页面自适应
-const onWindowResize = () => {
-  Camera.aspect = window.innerWidth / window.innerHeight;
-  Camera.updateProjectionMatrix();
-  Renderer.setSize(window.innerWidth, window.innerHeight);
-  renderThree();
-};
-window.addEventListener('resize', onWindowResize, false);
+// onMounted(() => {
+//   initCurrentThree();
+//   loadGltfModel();
+//   renderThree();
+// });
 
-// 外部模型加载函数
-const loadGltfModel = () => {
-  // 加载模型
-  let gltfLoader = new GLTFLoader();
-  gltfLoader.load(
-    '/src/assets/gltf/scene.gltf',
-    (gltf: any) => {
-      const loadscene = gltf.scene;
-      console.log(gltf);
-      window.gltf = gltf;
-      // gltf.position.set(100, 100, 100);
-      Scene.add(loadscene);
-      // 设置大小比例
-      loadscene.scale.set(0.06, 0.05, 0.05);
-      // 模型加载完,进行相机的初始化,传入设置的参数,模型加载为异步
-      cameraReset(cameraPosition, cameraLookat);
-    },
-    (xhr) => {
-      // 控制台查看加载进度xhr
-      console.log(Math.floor((xhr.loaded / xhr.total) * 100));
-    }
-  );
-};
+// const Scene = new THREE.Scene();
+// const Camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+// const Renderer = new THREE.WebGLRenderer({
+//   antialias: true,
+//   alpha: true //开启alpha
+// });
+// // 控制器
+// const Controls = new OrbitControls(Camera, Renderer.domElement);
+// //gltfLoader
+// const Gltfloader = new GLTFLoader();
+// // 注意:此处为threejs的DOM,需要将threejs的场景渲染进去
+// const threeDom = ref(null);
 
-const renderThree = () => {
-  requestAnimationFrame(renderThree);
-  Controls.update(); // 轨道控制器的更新
-  Renderer.clear(); // 清除画布
-  Renderer.render(Scene, Camera);
-};
+// // 首页进入相机的视角,这个视角可以在三维模型中建立一个摄像机获取摄像机的坐标,如C4D,非常准确.
+// const cameraPosition = {
+//   x: -50,
+//   y: 700,
+//   z: 0
+// };
+// const cameraLookat = {
+//   x: 0,
+//   y: 0,
+//   z: 1
+// };
+
+// // 声明一个方法传入参数可以在不同的地方调用相机
+// const cameraReset = (position, lookAt, time = 1) => {
+//   gsap.to(Camera.position, {
+//     x: position.x,
+//     y: position.y,
+//     z: position.z,
+//     duration: time,
+//     ease: 'power4.out'
+//     // onComplete: function () {
+//     // 这是相机运动完成的回调,可以执行其他的方法.
+//     // }
+//   });
+//   gsap.to(Camera.lookAt, {
+//     x: lookAt.x,
+//     y: lookAt.y,
+//     z: lookAt.z,
+//     duration: time,
+//     ease: 'power4.out'
+//   });
+//   gsap.to(Controls.target, {
+//     x: lookAt.x,
+//     y: lookAt.y,
+//     z: lookAt.z,
+//     duration: time,
+//     ease: 'power4.out'
+//   });
+// };
+
+// const initCurrentThree = () => {
+//   // 点光源
+//   const point = new THREE.PointLight(0xffffff, 1);
+//   point.position.set(10, 600, -40); // 点光源位置
+//   // Scene.position.set(0, 15, 0); // 场景位置
+//   // Scene.add(point); // 点光源添加到场景中
+//   // 环境光
+//   const ambient = new THREE.AmbientLight(0x444444, 1);
+//   Scene.add(ambient);
+//   // 辅助坐标系
+//   const axesHelper = new THREE.AxesHelper(500);
+//   // Scene.add(axesHelper);
+//   // 修改相机,场景的参数
+//   // Camera.position.set(18, 364, 397);
+//   // Camera.lookAt(0, 0, 0);
+//   // Camera.up.x = 0; //相机以哪个方向为上方
+//   // Camera.up.y = 1;
+//   // Camera.up.z = 1;
+//   Controls.target = new THREE.Vector3(0, 0, 0);
+//   // 使动画循环使用时阻尼或自转 意思是否有惯性
+//   Controls.enableDamping = true;
+//   // 动态阻尼系数 就是鼠标拖拽旋转灵敏度
+//   Controls.dampingFactor = 0.04;
+//   // 是否可以旋转
+//   Controls.enableRotate = false;
+//   // 是否可以缩放与速度
+//   Controls.enableZoom = false;
+//   // 设置相机距离原点的最远距离
+//   Controls.minDistance = 1;
+//   // 设置相机距离原点的最远距离
+//   Controls.maxDistance = 2000;
+//   // 是否开启右键拖拽
+//   Controls.enablePan = false;
+//   //render的相关设置
+//   Renderer.setPixelRatio(window.devicePixelRatio);
+//   Renderer.setSize(window.innerWidth, window.innerHeight);
+//   Renderer.inputEncoding = true;
+//   Renderer.outputEncoding = THREE.sRGBEncoding;
+//   Renderer.setClearColor(0xd0d0d0, 1);
+//   // 将renderer渲染进DOM里面
+//   threeDom.value.appendChild(Renderer.domElement);
+// };
+
+// // 设置页面自适应
+// const onWindowResize = () => {
+//   Camera.aspect = window.innerWidth / window.innerHeight;
+//   Camera.updateProjectionMatrix();
+//   Renderer.setSize(window.innerWidth, window.innerHeight);
+//   renderThree();
+// };
+// window.addEventListener('resize', onWindowResize, false);
+
+// const rotateAroundWorldAxis = (object, axis, radians) => {
+//   let rotWorldMatrix = new THREE.Matrix4();
+//   rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+//   rotWorldMatrix.multiply(object.matrix);
+//   object.matrix = rotWorldMatrix;
+//   object.rotation.setFromRotationMatrix(object.matrix);
+// };
+// // 外部模型加载函数
+// const loadGltfModel = () => {
+//   // 加载模型
+//   let gltfLoader = new GLTFLoader();
+//   gltfLoader.load(
+//     '/src/assets/gltf/scene.gltf',
+//     (gltf: any) => {
+//       const loadscene = gltf.scene;
+//       // gltf.position.set(100, 100, 100);
+//       Scene.add(loadscene);
+//       // 设置大小比例
+//       // loadscene.scale.set(0.02, 0.02, 0.02);
+//       // // 模型加载完,进行相机的初始化,传入设置的参数,模型加载为异步
+//       // cameraReset(cameraPosition, cameraLookat);
+//       let mroot = loadscene;
+//       let bbox = new THREE.Box3().setFromObject(mroot);
+//       console.log(bbox);
+//       let cent = bbox.getCenter(new THREE.Vector3());
+//       let size = bbox.getSize(new THREE.Vector3());
+
+//       // Rescale the object to normalized space
+//       let maxAxis = Math.max(size.x, size.y, size.z);
+//       mroot.scale.multiplyScalar(2.0 / maxAxis);
+//       bbox.setFromObject(mroot);
+//       bbox.getCenter(cent);
+//       bbox.getSize(size);
+//       //Reposition to 0,halfY,0
+//       mroot.position.copy(cent).multiplyScalar(-1);
+//       mroot.position.y -= size.y * 0.5;
+//       //调用方式，设置x、y、z轴的旋转
+//       let xAxis = new THREE.Vector3(1, 0, 0);
+//       let yAxis = new THREE.Vector3(0, 1, 0);
+//       let zAxis = new THREE.Vector3(0, 0, 1);
+//       //模型、旋转轴和旋转角度（弧度）
+//       rotateAroundWorldAxis(mroot, zAxis, Math.PI / 8);
+//     },
+//     (xhr) => {
+//       // 控制台查看加载进度xhr
+//       console.log(Math.floor((xhr.loaded / xhr.total) * 100));
+//     }
+//   );
+// };
+
+// const renderThree = () => {
+//   requestAnimationFrame(renderThree);
+//   Controls.update(); // 轨道控制器的更新
+//   Renderer.clear(); // 清除画布
+//   Renderer.render(Scene, Camera);
+// };
 </script>
 
 <style lang="scss" scoped>
--- src/views/client/three/utils/threeTool.ts
+++ src/views/client/three/utils/threeTool.ts
@@ -1,70 +1,217 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DragControls } from 'three/examples/jsm/controls/DragControls';
+import gsap from 'gsap';
 
-export default class CreateDivThree {
-  id: string;
-  ele: HTMLElement | null;
+export class CreateDivThree {
+  dom: string;
+  canvas: any;
   scene: any;
   camera: any;
   renderer: any;
   controls: any;
 
-  constructor(id: string) {
-    this.id = id;
-    this.ele = document.getElementById(id);
+  constructor(dom: string) {
+    this.dom = dom;
+    this.canvas = document.querySelector(dom);
+  }
+
+  init() {
+    this.initThree();
+    this.renderThree();
   }
 
   // 初始化场景
   initThree() {
-    let width = this.ele.offsetWidth;
-    let height = this.ele.offsetHeight;
+    let width = this.canvas.offsetWidth;
+    let height = this.canvas.offsetHeight;
     //  创建场景对象Scene
-    console.log(this.scene);
     this.scene = new THREE.Scene();
-    console.log(this.scene);
+    // 辅助三维坐标系
+    const axesHelper = new THREE.AxesHelper(500);
+    this.scene.add(axesHelper);
+    // 平行光
+    let directionalLight = new THREE.DirectionalLight(0xdfebff, 0.45);
+    directionalLight.position.set(50, 200, 100).normalize();
+    this.scene.add(directionalLight);
+    // 环境光
+    let ambientLight = new THREE.AmbientLight(0x999999);
+    this.scene.add(ambientLight);
     // 相机设置
-    this.camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 3000);
-    this.camera.position.z = 1;
+    this.camera = new THREE.PerspectiveCamera(45, width / height, 1, 2000);
+    this.camera.position.set(10, 150, 150);
+    this.camera.lookAt(this.scene.position);
+
     // 创建渲染器对象
     this.renderer = new THREE.WebGLRenderer({
+      // 是否执行抗锯齿
       antialias: true,
+      // 透明度
       alpha: true
     });
+    // 为了兼容高清屏幕
     this.renderer.setPixelRatio(window.devicePixelRatio);
+    // 改成这样就可以居中
     this.renderer.setSize(width, height);
-    this.ele.append(this.renderer.domElement);
-    this.renderer.setClearColor('#000');
-
-    // 三维坐标
-    this.scene.add(new THREE.AxesHelper(100));
-
-    // 灯光
-    let directionalLight = new THREE.DirectionalLight('#fff');
-    directionalLight.position.set(30, 30, 30).normalize();
-    this.scene.add(directionalLight);
-    let ambientLight = new THREE.AmbientLight('#fff', 0.3);
-    this.scene.add(ambientLight);
+    this.canvas.appendChild(this.renderer.domElement);
+    // this.renderer.setClearColor('#000');
 
     // 初始化控制器,创建控件对象
-    let controls = new OrbitControls(this.camera, this.renderer.domElement);
-    controls.enableDamping = true;
-    controls.enableZoom = true;
-    controls.autoRotate = false;
-    controls.autoRotateSpeed = 0.3;
-    controls.enablePan = true;
+    let controls = new TrackballControls(this.camera, this.renderer.domElement);
+    // 是否禁用缩放
+    controls.noZoom = false;
+    // 是否禁用平移
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    controls.target = new THREE.Vector3(2, 44, -32);
     this.controls = controls;
+  }
 
-    window.addEventListener(
-      'resize',
-      () => {
-        console.log('resize');
-        this.camera.aspect = this.ele.offsetWidth / this.ele.offsetWidth;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(this.ele.offsetWidth, this.ele.offsetWidth);
+  renderThree = () => {
+    requestAnimationFrame(this.renderThree);
+    this.controls.update(); // 轨道控制器的更新
+    this.renderer.clear(); // 清除画布
+    this.renderer.render(this.scene, this.camera);
+  };
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  windowResize = () => window.addEventListener('resize', () => this.resize());
+
+  // 声明一个方法传入参数可以在不同的地方调用相机
+  cameraReset(position: any, lookAt: any, time = 1) {
+    gsap.to(this.camera.position, {
+      x: position.x,
+      y: position.y,
+      z: position.z,
+      duration: time,
+      ease: 'power4.out'
+      // onComplete: function () {
+      // 这是相机运动完成的回调,可以执行其他的方法.
+      // }
+    });
+    gsap.to(this.camera.lookAt, {
+      x: lookAt.x,
+      y: lookAt.y,
+      z: lookAt.z,
+      duration: time,
+      ease: 'power4.out'
+    });
+    gsap.to(this.camera.target, {
+      x: lookAt.x,
+      y: lookAt.y,
+      z: lookAt.z,
+      duration: time,
+      ease: 'power4.out'
+    });
+  }
+}
+
+// 加载3D模型类
+export class LoadGltfModel {
+  path: string;
+  modelName: string;
+  scene: any;
+  camera: any;
+  modelScene: any;
+
+  constructor(path: string, modelName: string, scene: any, camera: any) {
+    this.path = path;
+    this.modelName = modelName;
+    this.scene = scene;
+    this.camera = camera;
+  }
+
+  loadModel() {
+    // 加载模型
+    let gltfLoader = new GLTFLoader();
+    gltfLoader.setPath(this.path);
+    gltfLoader.load(
+      this.modelName,
+      (gltf: any) => {
+        const loadscene = gltf.scene;
+        loadscene.children[0].geometry.computeBoundingBox();
+        loadscene.children[0].geometry.center();
+        let helper = new THREE.BoundingBoxHelper(loadscene, 0xff0000);
+        helper.update();
+        this.scene.add(helper);
+        this.scene.add(loadscene);
+        gltf.rotation.x = THREE.Math.degToRad(90);
+        // loadscene.rotate.set(180, 0, 0);
+        this.modelScene = loadscene;
+        // 设置大小比例
+        loadscene.scale.set(100, 100, 100);
+        this.scene.add(loadscene);
       },
-      false
+      (xhr: any) => {
+        // 控制台查看加载进度xhr
+        console.log(Math.floor((xhr.loaded / xhr.total) * 100));
+      },
+      (error: any) => {
+        // 加载出错
+        console.log(error);
+      }
     );
+  }
+
+  // 动态设置模型比例大小
+  adjustModel(dom: any, modelScene: any) {
+    let divDom = document.querySelector(dom);
+    let width = divDom.offsetWidth;
+    console.log(width);
+    let height = divDom.offsetHeight;
+    let group = new THREE.Group();
+    group.add(modelScene);
+    let bbox = new THREE.Box3().setFromObject(group);
+    // 边界的最小坐标值 边界的最大坐标值
+    let mdlen = bbox.max.x - bbox.min.x;
+    let mdhei = bbox.max.y - bbox.min.y;
+    let mdwid = bbox.max.z - bbox.min.z;
+    group.position.set(0, 0, 0);
+    console.log(bbox);
+    console.log(this.camera.position.z);
+    let dist = Math.abs(this.camera.position.z - group.position.z - mdwid / 2);
+    console.log('dist值为:' + dist);
+    let vFov = (this.camera.fov * Math.PI) / 180;
+    //console.log('vFov值为:' + vFov );
+    let vheight = 2 * Math.tan(vFov * 0.5) * dist;
+    //console.log('vheight值为:' + vheight );
+    let fraction = mdhei / vheight;
+    // console.log('fraction值为:' + fraction );
+    let finalHeight = height * fraction;
+    //console.log('finalHeight值为:' + finalHeight);
+    let finalWidth = (finalHeight * mdlen) / mdhei;
+    //console.log('finalWidth值为:' + finalWidth );
+
+    let value1 = width / finalWidth;
+    console.log('value1缩放比例值为:' + value1);
+    let value2 = height / finalHeight;
+    console.log('value2缩放比例值为:' + value2);
+
+    if (value1 >= value2) {
+      group.scale.set(value2, value2, value2);
+    } else {
+      group.scale.set(value1, value1, value1);
+    }
+    let bbox2 = new THREE.Box3().setFromObject(group);
+    let mdlen2 = bbox2.max.x - bbox2.min.x;
+    let mdhei2 = bbox2.max.y - bbox2.min.y;
+    let mdwid2 = bbox2.max.z - bbox2.min.z;
+    group.position.set(
+      -(bbox2.max.x + bbox2.min.x) / 2,
+      -(bbox2.max.y + bbox2.min.y) / 2,
+      -(bbox2.max.z + bbox2.min.z) / 2 - (bbox2.max.z - bbox2.min.z) / 2
+    );
+
+    this.scene.add(group);
+
+    let boxhelper = new THREE.BoxHelper(group, 0xbe1915); //外面红色框
+    this.scene.add(boxhelper);
   }
 }
